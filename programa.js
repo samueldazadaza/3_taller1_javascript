7d7acf5b023c5398d60c34b1dd1ef80e4f0c27ae
sam <samueldazadaza@gmail.com>
2023-03-27 18:21:43 -0500

    fin clase 3mar2023

---
+++ programa.js
@@ -0,0 +1,61 @@
+// alert("hola javascript");
+
+//console.log("Hola mundo!!!");
+
+// JS es No tipado: no define el tipo de dato que va a guardar en las variables.
+
+ let a = 3;
+ let b = 8;
+ let c = "3"
+
+ console.log(a%b);
+
+ //Aritmeticas:  * + - / %
+
+ //Comparación: (condiciones)
+ // > < >= <= == === !=
+ //Siempre devuelven un valor booleano Tue o False
+
+ console.log(a > b);
+ console.log(b == c);
+ console.log(b === c);
+ console.log(b + c); //casting automatico: conversion automatica del tipo de dato.
+
+ //operaciones lógicas: AND OR
+ // AND: &&
+ // OR: ||
+
+ a = true;
+ b = false;
+ console.log(a && b);
+ console.log(a || b);
+
+ // Los programas son secuenciales
+ // Linea por linea
+
+ // Romper la secuencialidad del programa para evitar procesar ciertas lineas de codigo.
+
+ // Sentencias de desición:
+ // if(condición){}
+
+ if (a > b){
+    console.log("variable A es mayor que b");
+ }
+
+ // if(condición){} else{};
+
+ if(a > b){
+    console.log("letra a es mayor que b");
+ }else{
+    console.log("la letra (A) es menor que b")
+ }
+
+ //Talleres para el dia lunes:
+ 
+ // Taller: Definir dos variables, asignarles un valor y utilizando decisiones imprimir en la consola la que tenga menor valor
+
+ // Taller: Definir tres variables, asignarles valores y utilizar decisiones para imprimir en la consola la que tenga el mayor valor
+
+
+
+
---
+++ taller1javascript.js
@@ -0,0 +1,5 @@
+ //Talleres para el dia lunes:
+ 
+ // Taller: Definir dos variables, asignarles un valor y utilizando decisiones imprimir en la consola la que tenga menor valor
+
+ // Taller: Definir tres variables, asignarles valores y utilizar decisiones para imprimir en la consola la que tenga el mayor valor
